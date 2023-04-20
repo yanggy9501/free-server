00bf9d9920d0e8fc544168164b6353850bed3a10
yanggy <ilike.yanggy@gmail.com>
2023-04-20 14:27:44 +0800

    id

---
+++ freeing-authercent-jwt/src/main/java/com/freeing/authcenterv2/controller/LoginController.java
@@ -0,0 +1,42 @@
+package com.freeing.authcenterv2.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Mr.M
+ * @version 1.0
+ * @description 测试controller
+ * @date 2022/9/27 17:25
+ */
+@Slf4j
+@RestController
+public class LoginController {
+
+    @RequestMapping("/login-success")
+    public String loginSuccess() {
+        return "登录成功";
+    }
+
+    @RequestMapping("/user/{id}")
+    public String getuser(@PathVariable("id") String id) {
+        return "xcUser" + id;
+    }
+
+    @RequestMapping("/r/r1")
+    @PreAuthorize("hasAuthority('p1')")
+    public String r1() {
+        return "访问r1资源";
+    }
+
+    @RequestMapping("/r/r2")
+    @PreAuthorize("hasAuthority('p2')")
+    public String r2() {
+        return "访问r2资源";
+    }
+
+
+}
